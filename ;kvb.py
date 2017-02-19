9e201d2f773db8165db4922c67e95518603bba76
DadaTheGeek <Davidakadamov@gmail.com>
2017-02-19 23:04:47 +0100

    Add files via upload

---
+++ ;kvb.py
@@ -0,0 +1,14 @@
+print('prenom:')
+prenom = input()
+print('mot de passe:')
+mdp =input()
+while(prenom != 'David') or (mdp != 'kaka'):
+    print('PRENOM OU MOT DE PASSE INCORECTE')
+    print('prenom:')
+    prenom = input()
+    print('mot de passe:')
+    mdp =input()
+
+else:
+    print('validé')
+    input()
